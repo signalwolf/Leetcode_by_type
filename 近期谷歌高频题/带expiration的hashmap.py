2dfe64f6862c47e82ec716c78a09705ced6da219
signalwolf <XuHanSD@gmail.com>
2018-10-27 22:57:37 -0700

    Update 近期谷歌高频题/带expiration的hashmap.py

--- 近期谷歌高频题/带expiration的hashmap.py
+++ 近期谷歌高频题/带expiration的hashmap.py
@@ -6,20 +6,22 @@ class hashmap(object):
         self.hash = {}
         self.heap = []
 
-
+    # time complexity: O(1)
     def get(self, key, time):
-        value, expire_time = self.hash[key]
+        # value, expire_time = self.hash[key]
         self.clean(time)
         if key in self.hash:
             return self.hash[key]
         else:
             return None
 
+    # time complexsity: O(len(heap)) --> O(N)
     def clean(self, time):
         while len(self.heap) > 0 and self.heap[0][0] < time:
             _, key = heappop(self.heap)
             del self.hash[key]
 
+    # O(logN)
     def set(self, key, value, expire):
         self.hash[key] = (value, expire)
         heappush(self.heap, (expire, key))
@@ -30,6 +32,6 @@ for i in xrange(10):
     curr_time = randint(0, 10)
     hash.set(i, i, curr_time)
 print hash.hash
-hash.clean(20)
+hash.clean(5)
 print hash.hash
 
