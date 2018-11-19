020668d427498847d6aab3c198c38252c311dd7a
signalwolf <XuHanSD@gmail.com>
2018-11-18 20:57:07 -0800

    update

---
+++ MS Leetcode喜提/店面准备MS/面经/285. Inorder Successor in BST.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderSuccessor(self, root, p):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :rtype: TreeNode
+        """
+
+        # succ = None
+        # while root:
+        #     if p.val < root.val:
+        #         succ = root
+        #         root = root.left
+        #     else:
+        #         root = root.right
+        # return succ
+        curr = root
+        succ = None
+        while curr:
+            if curr.val > p.val:
+                succ = curr
+                curr = curr.left
+            else:
+                curr = curr.right
+        return succ
