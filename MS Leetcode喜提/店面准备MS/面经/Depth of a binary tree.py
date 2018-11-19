aca12e88f7277132c7a1b0cd642df32c0a7764d6
signalwolf <XuHanSD@gmail.com>
2018-11-18 17:27:52 -0800

    update

--- MS Leetcode喜提/店面准备MS/面经/Depth of a binary tree.py
+++ MS Leetcode喜提/店面准备MS/面经/Depth of a binary tree.py
@@ -1,6 +1,22 @@
-
+from collections import deque
 def depthofBinaryTree(root):
     # BFS, DFS
-    # DFS:
+    # DFS: 36ms
     if root == None: return 0
     return max(depthofBinaryTree(root.left), depthofBinaryTree(root.right)) + 1
+
+    # BFS:32ms
+    if root == None: return 0
+    depth = 0
+    queue = deque([root])
+    while queue:
+        next_level = deque()
+        depth += 1
+        while queue:
+            curr = queue.popleft()
+            if curr.left:
+                next_level.append(curr.left)
+            if curr.right:
+                next_level.append(curr.right)
+        queue = next_level
+    return depth
