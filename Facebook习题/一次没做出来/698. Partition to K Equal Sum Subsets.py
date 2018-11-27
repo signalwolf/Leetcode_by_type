6f9bc3d468dd2c6ca412d46bebf80ed77a4b8ead
signalwolf <XuHanSD@gmail.com>
2018-11-26 21:38:06 -0800

    update

--- Facebook习题/一次没做出来/698. Partition to K Equal Sum Subsets.py
+++ Facebook习题/一次没做出来/698. Partition to K Equal Sum Subsets.py
@@ -30,3 +30,31 @@ class Solution(object):
         nums.sort(reverse=True)
         return self.helper(nums, 0, [0] * k, target)
 
+# rewrite:
+class Solution(object):
+
+    def backtracking(self, nums, index, target, sums):
+        if index == len(nums):
+            return True
+
+        # print sums, target
+        for j, currSum in enumerate(sums):
+            if currSum + nums[index] != target and currSum + nums[index] + nums[-1] > target:
+                continue
+            sums[j] = currSum + nums[index]
+            if self.backtracking(nums, index + 1, target, sums):
+                return True
+            sums[j] = currSum
+        return False
+
+    def canPartitionKSubsets(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        total = sum(nums)
+        if total % k != 0 or len(nums) == 0: return False
+        nums.sort(reverse=True)
+        return self.backtracking(nums, 0, total / k, [0] * k)
+
